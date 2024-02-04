3f5ebbc2c7948c080a4e9b53d310473c159414d3
Mohammad Baqir <78399170+m-baqir@users.noreply.github.com>
2024-02-04 14:33:14 -0500

    initial setup
    initial setup complete. hello world controller working

---
+++ src/test/java/com/AnimalLoversSociety/AnimalLoversSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnimalLoversSociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalLoversSocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
