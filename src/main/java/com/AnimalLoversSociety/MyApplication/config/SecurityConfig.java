7a166f2dd527b8ba80c1dce9f087a53c5cd6b098
Mohammad Baqir <78399170+m-baqir@users.noreply.github.com>
2024-03-29 11:43:57 -0400

    security implemented
    login security implemented on list of users page

--- src/main/java/com/AnimalLoversSociety/MyApplication/config/SecurityConfig.java
+++ src/main/java/com/AnimalLoversSociety/MyApplication/config/SecurityConfig.java
@@ -2,32 +2,48 @@ package com.AnimalLoversSociety.MyApplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import com.AnimalLoversSociety.MyApplication.user.CustomUserDetailsService;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .anyRequest().authenticated())
-                .httpBasic(withDefaults());
-        return http.build();
+    UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.authenticationProvider(authenticationProvider());
+
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/users").authenticated()
+                .anyRequest().permitAll())
+                .formLogin(login -> login.usernameParameter("username")
+                        .defaultSuccessUrl("/users")
+                        .permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/").permitAll());
+
+        return http.build();
+    }
 }
-/*
- * .requestMatchers("/users").hasRole("ADMIN")
- * .requestMatchers("/index").permit
- */
--- src/main/java/com/AnimalLoversSociety/MyApplication/user/UserRepository.java
+++ src/main/java/com/AnimalLoversSociety/MyApplication/user/UserRepository.java
@@ -1,7 +1,9 @@
 package com.AnimalLoversSociety.MyApplication.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    public User findByUsername(String username);
 }
