472c362ae815fabab54999cd5b2ea391144ecceb
aporporo <117672857+aporporo@users.noreply.github.com>
2024-04-01 21:41:41 -0400

    fixed donor repository so I could run

--- src/main/java/com/AnimalLoversSociety/MyApplication/donors/donorRepository.java
+++ src/main/java/com/AnimalLoversSociety/MyApplication/donors/donorRepository.java
@@ -1,8 +1,11 @@
 package com.AnimalLoversSociety.MyApplication.donors;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Transactional
