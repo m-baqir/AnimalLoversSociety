b4b478de3ed0c251c86be8fae7eb860ef56e48a6
Arsh <159047826+0arsh1@users.noreply.github.com>
2024-04-05 01:28:48 -0400

    Update DonationsController.java

--- src/main/java/com/AnimalLoversSociety/MyApplication/donations/DonationsController.java
+++ src/main/java/com/AnimalLoversSociety/MyApplication/donations/DonationsController.java
@@ -1,50 +1,16 @@
 package com.AnimalLoversSociety.MyApplication.donations;
 
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-import java.util.Optional;
-
-@RestController
-public class DonationsController {
-
-    @Autowired
-    private DonationsRepository donationsRepo;
-
-    @GetMapping(path="/donations/all")
-    public Iterable<Donations> getAllDonations() { return donationsRepo.findAll(); }
-
-    @GetMapping(path="/donations/all/{id}")
-    public Optional<Donations> show(@PathVariable String id) {
-        long donationId = Long.parseLong(id);
-        return donationsRepo.findById(donationId);
-    }
-
-    //Delete method is not working for some reason
-    @RequestMapping(value = "/donations/del/{donationId}", method = {RequestMethod.POST, RequestMethod.GET})
-    public @ResponseBody String removeDonations(@PathVariable long donationId) {
-        donationsRepo.deleteById(0L);
-
-        return "Deleted";
-    }
-
-    @RequestMapping(value = "/donations/{donationId}", method = {RequestMethod.POST, RequestMethod.GET})
-    public @ResponseBody String addNewDonation(@PathVariable long donationId) {
-        Donations n = new Donations();
-        n.setDonationAmount(666.66);
-        n.setDate(new Date());
-        n.setDonationID(donationId);
-        n.setDonorID(1111);
-        donationsRepo.save(n);
-        return "Saved Donation";
-    }
-
-    @GetMapping(path="/donations/test")
-    public String testString() { return "THIS IS A TEST FROM DONATIONS"; }
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
+@Repository
+@Transactional
+public interface DonationsRepository extends JpaRepository<Donations, Integer>  {
+    @Query
+    List<Donations> findBydonorID(int donorID);
 }
-
-
