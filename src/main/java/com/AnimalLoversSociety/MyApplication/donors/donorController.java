fb7e05896cb462318da911f30834ad37c176fa4e
Arsh <159047826+0arsh1@users.noreply.github.com>
2024-04-05 17:13:24 -0400

    Update donorController.java (#47)

--- src/main/java/com/AnimalLoversSociety/MyApplication/donors/donorController.java
+++ src/main/java/com/AnimalLoversSociety/MyApplication/donors/donorController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class donorController {
@@ -21,14 +24,28 @@ public class donorController {
     public String showDonorAdd(Model model) {
         Iterable<donor> donor1 = donorRepo.findAll();
         model.addAttribute("donor", donor1);
+
         return "donors_add";
     }
 
-    //Edit donor
-    @GetMapping("/donors/edit")
-    public String showDonorEdit(Model model) {
-        Iterable<donor> donor1 = donorRepo.findAll();
-        model.addAttribute("donor", donor1);
-        return "donors_edit";
+    @PostMapping("/donors")
+    public String saveItem(@ModelAttribute("donors") donor donor1,
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "address", required = false) String address,
+            @RequestParam(value = "gender", required = false) String gender,
+            @RequestParam(value = "donation") String donation,
+            @RequestParam(value = "name_change", required = false) String name_change,
+            @RequestParam(value = "member", required = false) String member) {
+
+                donorRepo.save(donor1);
+                return "redirect:/donors";
     }
+
+    //Edit donor
+    // @GetMapping("/donors/edit")
+    // public String showDonorEdit(Model model) {
+    //     Iterable<donor> donor1 = donorRepo.findAll();
+    //     model.addAttribute("donor", donor1);
+    //     return "donors_edit";
+    // }
 }
