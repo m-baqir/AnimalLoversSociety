7974457c0cacbd0dfcbc1edb86867acc5a397b42
Arsh <159047826+0arsh1@users.noreply.github.com>
2024-04-05 17:17:02 -0400

    Update DonationsRepository.java (#52)

--- src/main/java/com/AnimalLoversSociety/MyApplication/donations/DonationsRepository.java
+++ src/main/java/com/AnimalLoversSociety/MyApplication/donations/DonationsRepository.java
@@ -1,12 +1,15 @@
 package com.AnimalLoversSociety.MyApplication.donations;
 
-import com.AnimalLoversSociety.MyApplication.items.Items;
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Repository
 @Transactional
-public interface DonationsRepository extends CrudRepository<Donations, Long> {
+public interface DonationsRepository extends JpaRepository<Donations, Integer>  {
+    @Query
+    List<Donations> findByDonationID(long id);
 }
