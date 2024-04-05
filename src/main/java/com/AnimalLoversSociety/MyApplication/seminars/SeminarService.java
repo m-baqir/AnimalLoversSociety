d151b1583c4a92299bb1fa23a4d2cb6dafc60010
em-au <emilyau10@gmail.com>
2024-04-05 17:17:57 -0400

    fixed seminar delete function when seminar has attendees

--- src/main/java/com/AnimalLoversSociety/MyApplication/seminars/SeminarService.java
+++ src/main/java/com/AnimalLoversSociety/MyApplication/seminars/SeminarService.java
@@ -1,6 +1,5 @@
 package com.AnimalLoversSociety.MyApplication.seminars;
 
-import com.AnimalLoversSociety.MyApplication.employees.Employees;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
